638d5987e2e1d44402eb9ad5a8b887f71165e5ea
maiconio <maiconscosta@gmail.com>
2015-05-28 00:20:23 -0300

    Send fake JSON payload to 127.0.0.1:8080

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	fmt.Printf("%v", payload)
+	buf := strings.NewReader(payload)
+	resp, err := http.Post("http://127.0.0.1:8080", "application/json", buf)
+	fmt.Printf("%v\n", resp)
+	fmt.Printf("%v\n", err)
 }
