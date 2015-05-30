93c24a76b0abb0bdf968d21da56ef3cb4db40d03
maiconio <maiconscosta@gmail.com>
2015-05-29 23:55:18 -0300

    parametrize the payload file

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 )
 
 var targetHost string
+var payloadFile string
 
 func init() {
 	flag.StringVar(&targetHost, "host", "", "The target host")
+	flag.StringVar(&payloadFile, "file", "", "The payload file")
 	flag.Parse()
 }
 
@@ -17,11 +21,15 @@ func main() {
 	if targetHost == "" {
 		flag.Usage()
 	} else {
-		sendPayload(targetHost)
+		sendPayload(targetHost, payloadFile)
 	}
 }
 
-func sendPayload(url string) (resp *http.Response, err error) {
-	buf := strings.NewReader(payload)
+func sendPayload(url string, file string) (resp *http.Response, err error) {
+	fileContents, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf := strings.NewReader(string(fileContents))
 	return http.Post(url, "application/json", buf)
 }
--- main_test.go
+++ main_test.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestSendPayload(t *testing.T) {
+	testFile := "payload.json"
+
 	echoServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		body, _ := ioutil.ReadAll(r.Body)
 		fmt.Fprintf(w, string(body))
 	}))
 	defer echoServer.Close()
 
-	resEchoServer, err := sendPayload(echoServer.URL)
+	resEchoServer, err := sendPayload(echoServer.URL, testFile)
 
 	if err != nil {
 		t.Fatalf("Error sending payload to echoServer: %v", err)
@@ -27,7 +30,12 @@ func TestSendPayload(t *testing.T) {
 		t.Fatalf("Error receiving payload response from EchoServer: %v", err)
 	}
 
-	if string(respPayload) != payload {
+	fileContents, err := ioutil.ReadFile(testFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if string(respPayload) != string(fileContents) {
 		t.Fatal("Payload sent and received from EchoServer are not equals.")
 	}
 }
